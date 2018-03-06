7001e787b2deaed37138de67685636e37b11241d
Bryan Miller <bryanmiller89@gmail.com>
2018-03-05 18:45:23 -0800

    Create makeGrid() function

--- designs.js
+++ designs.js
@@ -14,6 +14,15 @@ $('#sizePicker').submit(function (event) {
     console.log(`Height: ${height}, Width: ${width}`);
 })
 
-function makeGrid() {
-    
+function makeGrid(x, y) {
+    // Remove previous grid if one exists
+    $('tr').remove();
+    // For loop to create table rows
+    for (var i = 1; i <= x; i++) {
+        $('#pixelCanvas').append('<tr id=table' + i + '></tr>');
+        // Nested for loop to create table columns
+        for (var j = 1; j <= y; j++) {
+            $('#table' + i).append('<td></td>');
+        }
+    }
 }
